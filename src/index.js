9006b07fe6e19d5c2a7ee4c7f68372e1a10de391
Prashant Bhushan Shukla <59869762+prashantshukla2000@users.noreply.github.com>
2022-11-29 04:19:34 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,53 @@
+//Map
+// var numbers = [3, 56, 2, 48, 5];
+// function double(x) {
+//   return x * 2;
+// }
+// const Newnumbers = numbers.map(double);
+// console.log(Newnumbers);
+
+//filter
+// var numbers = [3, 56, 2, 48, 5];
+// var newNumbers=numbers.filter(function (num) {
+//  return num > 10;
+// });
+// console.log(newNumbers);
+
+//Reduce
+// var numbers = [3, 56, 2, 48, 5];
+// var newNumber = numbers.reduce(function (accumalator, currentNumber) {
+//   return accumalator + currentNumber;
+// });
+// console.log(newNumber);
+
+//Find
+// const array1 = [5, 12, 8, 130, 44];
+// const found = array1.find(element => element > 10);
+// console.log(found);
+
+//FindIndex
+// const array1 = [5, 12, 8, 130, 44];
+// const found = array1.findIndex(function (element) {
+//   return element > 10;
+// });
+// console.log(found);
+// // expected output: 12
+
+//Map -Create a new array by doing something with each item in an array.
+
+//Filter - Create a new array by keeping the items that return true.
+
+//Reduce - Accumulate a value by doing something to each item in an array.
+
+//Find - find the first item that matches from an array.
+
+//FindIndex - find the index of the first item that matches.
+
+//Challenge to cut down emojipedia.meanings to 100 words only...
+// import emojipedia from "./emojipedia";
+// console.log(emojipedia);
+
+// var newmeaning=emojipedia.map(function(emojiEntry){
+//   return emojiEntry.meaning.substring(0, 100);
+// });
+// console.log(newmeaning);
